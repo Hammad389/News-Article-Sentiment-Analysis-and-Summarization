27747a8c052ea8dbc367043a64818f69a64ac78b
HAMMAD <107891006+Hammad389@users.noreply.github.com>
2025-05-27 10:53:17 +0500

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -59,3 +59,30 @@ class ApartmentData(Base_multiple_table):
     Min_rent = Column(Float, nullable=True)
     amenities = Column(Text, nullable=True)
     community_amenities = Column(Text, nullable=True)
+
+
+
+"C:\Users\Hammadhussain\PycharmProjects\Module5\.venv\Lib\site-packages\mysql\connector\connection_cext.py", line 781, in cmd_query
+    raise get_mysql_exception(
+sqlalchemy.exc.DatabaseError: (mysql.connector.errors.DatabaseError) 1822 (HY000): Failed to add the foreign key constraint. Missing index for constraint 'apartment_data_ibfk_1' in the referenced table 'community_data'
+[SQL:
+CREATE TABLE apartment_data (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        community VARCHAR(100),
+        apartment_no VARCHAR(50),
+        no_of_bedrooms INTEGER,
+        no_of_bathrooms INTEGER,
+        area INTEGER,
+        floor_no INTEGER,
+        availability BOOL,
+        deposit FLOAT,
+        `Max_rent` FLOAT,
+        `Min_rent` FLOAT,
+        amenities TEXT,
+        community_amenities TEXT,
+        PRIMARY KEY (id),
+        FOREIGN KEY(community) REFERENCES community_data (community_name)
+)
+
+]
+(Background on this error at: https://sqlalche.me/e/20/4xp6)
